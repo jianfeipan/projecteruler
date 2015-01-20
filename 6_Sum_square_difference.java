31462239da88f7f097440e61c06381cbb21a1bba
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-20 17:04:42 +0100

    Update 6_Sum_square_difference.java

--- 6_Sum_square_difference.java
+++ 6_Sum_square_difference.java
@@ -5,5 +5,12 @@ The square of the sum of the first ten natural numbers is,
 Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.*/
 public class Sum_square_difference{
-
+  /*sum of squares : n(n+1)(2n+1)/6, sum : (1+n)n/2*/
+  public static int sumSquare(int max){
+    reture max*(max+1)*(2*max+1)/6;
+  }
+  
+  public static int sum(int max){
+    return max*(max+1)/2;
+  }
 }
