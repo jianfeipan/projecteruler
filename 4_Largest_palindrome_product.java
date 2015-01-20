919ab00445b06de529182f160e266ab45dc38939
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-20 15:39:53 +0100

    Update 4_Largest_palindrome_product.java

--- 4_Largest_palindrome_product.java
+++ 4_Largest_palindrome_product.java
@@ -4,4 +4,29 @@ public class Largest_palindrome_product{
   public static void find3digitproductPalindrom(){
      
   }
+  
+  public int[] intToarray(int number){
+    int count=0;
+    int temp=number;
+    while(temp>0){
+      temp/=10;
+      count++;
+    }
+    int[] result=new int[count];
+    temp=number;
+    for(int i =0; i<count;i++){
+      result[i]=temp%10;
+      temp/=10;
+    }
+    return result;
+  }
+  
+  public boolean isPalindrom(int[] intarray){
+    int len=int_array.length;
+    for(int i=0; i<len/2;i++){
+      if(intarray[i]!=intarray[len-1-i])
+        return false;
+    }
+    return true;
+  }
 }
